19b65de565732f09f559f7ad90c8bd5925f7fcd0
Johann Lee <me@qinka.pro>
2018-08-28 15:38:30 +0800

    Add Travis CI script of hQinkaCS.

--- .ci/travis_ci_script.sh
+++ .ci/travis_ci_script.sh
@@ -81,14 +81,72 @@ function qinkacs_succ() {
 
 ## hQinkaCS
 function hQinkaCS_init() {
-    #FAI_init
+    sudo $APT install -y ghc-$GHC_VER 
+
+    if [ -n "$LLVM" ]; then
+        echo
+        echo Using llvm-$LLVM
+        echo
+        sudo $APT install -y lldb-$LLVM llvm-$LLVM llvm-$LLVM-dev llvm-$LLVM-runtime
+    fi
+
+    #
+    # I was planning enable the CUDA test, but the Travis-CI do not support such things.
+    # So it's abandoned now.
+    #
+    if [ -n "$CUDA" ]; then
+        echo CUDA should be disable.
+        exit 3
+    fi
+
+    echo Setting up ghc-$GHC_VER
+    export PATH=/opt/ghc/$GHC_VER/bin:$PATH
+    ghc -V
+
+    mkdir -p $HOME/.local/bin
+    export PATH=$HOME/.local/bin:$PATH
+    travis_retry curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
+
+
+    echo Configuring
+    stack config set system-ghc --global true
     qinkacs_init
-    #FAI_build
-    qinkacs_build
 }
 
 function hQinkaCS_build() {
-    stack build $FLAGS hQinkaCS
+    echo hQinkaCS task
+    ## LLVM
+    if [ -n "$LLVM" ]; then
+        echo Enable LLVM
+        export X_LLVM_FLAGS=" --ghc-options -fllvm --ghc-options -pgmlo --ghc-options opt-$LLVM --ghc-options -pgmlc --ghc-options llc-$LLVM "
+    fi
+
+    ## THREADED
+    if [ -n "$THREADED" ]; then
+        echo Enable -threaded
+        export X_THREADED_FLAGS=" --ghc-options -threaded --ghc-options -with-rtsopts=-N "
+    fi 
+
+    ## 
+    if [ -n "$DEBUG" ]; then
+        echo Enable debug
+        export X_DEBUG_FLAGS=" --ghc-options -rtsopts=all --ghc-options -g"
+    else
+        export X_DEBUG_FLAGS=" --ghc-options -rtsopts=some --ghc-options --ghc-options --ghc-options -O3"
+    fi
+
+    if [ -n "$CUDA" ]; then
+        echo Enable CUDA
+        export X_CUDA_FLAGS=" "
+    else
+        echo Disable CUDA
+        export X_CUDA_FLAGS=" --flag FAI:-enable-cuda "
+    fi
+
+    cd $TRAVIS_BUILD_DIR
+    export FLAGS="$X_THREADED_FLAGS $X_LLVM_FLAGS $X_DEBUG_FLAGS $X_CUDA_FLAGS"
+    echo Using flags: $FLAGS
+    stack build $FLAGS hQinkaCS hQinkaCS-binding
 }
 
 function hQinkaCS_test() {
