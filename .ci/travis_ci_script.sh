bc16b4a8469eb1768c82218da6abcd11b03a9083
Johann Lee <me@qinka.pro>
2018-08-28 10:48:20 +0800

    Update [PRODUCT BACKLOG ITEM 15]
    1. Add the ci script

---
+++ .ci/travis_ci_script.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+# Copyright (C) 2018 Johann Lee <me@qinka.pro>
+## Command
+function common_init() {
+    echo
+    echo Pre-install
+    echo
+
+    echo Fetching the system\' name
+    export OS_CORENAME=$(lsb_release -c | awk '{print $2}')
+    export OS_DISTRIBUTOR=$(lsb_release -i | awk '{print $3}')
+    echo Using $OS_DISTRIBUTOR $OS_CORENAME for building
+
+    export APT=apt-get
+    echo Updating $APT source
+    sudo $APT update
+}
+
+
+## Qinka Computing Subprogram
+function qinkacs_init() {
+    echo
+    echo Init for Qinka Computing Subprogram
+    echo
+    echo Build gtest
+    cd /usr/src/gtest/
+    sudo cmake -DCMAKE_INSTALL_PREFIX=/usr/ .
+    sudo make
+    sudo find .
+}
+
+function qinkacs_build() {
+    echo
+    echo build Qinka Computing Subprogram
+    cd $TRAVIS_BUILD_DIR
+    mkdir build
+    cd build
+    export QCS_BUILD_DIR=`pwd`
+
+    local TEST_STATUS ACC_CMAKE_FLAGS
+    # test enable
+    if [ -n "$RUN_TEST" ]; then
+        TEST_STATUS=On
+    else
+        TEST_STATUS=Off
+    fi
+
+    case $ACC_BACKEND in
+        OpenMP)
+            ACC_CMAKE_FLAGS="-DENABLE_OPENMP=On"
+            ;;
+        OpenMP-target)
+            ACC_CMAKE_FLAGS="-DENABLE_OPENMP=On -DENABLE_OPENMP_TARGET=On"
+            ;;
+        OpenACC)
+            ACC_CMAKE_FLAGS="-DENABLE_OPENMP=Off -DENBALE_OPENACC=On"
+            ;;
+        *)
+            ACC_CMAKE_FLAGS="-DENABLE_OPENMP=Off"
+            ;;
+    esac
+
+
+    cmake -DBUILD_TESTS=$TEST_STATUS $ACC_CMAKE_FLAGS -DGTEST_ROOT=/usr/src/gtest ..
+    cmake --build .
+
+    cd $TRAVIS_BUILD_DIR
+}
+
+function qinkacs_test() {
+    echo
+    echo Run testing of Qinka Computing Subprogram
+    cd $QCS_BUILD_DIR
+    ctest -V
+}
+
+function qinkacs_succ() {
+    echo "Nothing to do"
+}
+
+## hQinkaCS
+function hQinkaCS_init() {
+    #FAI_init
+    qinkacs_init
+    #FAI_build
+    qinkacs_build
+}
+
+function hQinkaCS_build() {
+    stack build $FLAGS hQinkaCS
+}
+
+function hQinkaCS_test() {
+    echo "Nothing to do"
+}
+
+function hQinkaCS_succ() {
+    echo "Nothing to do"
+}
